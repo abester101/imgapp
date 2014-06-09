cd37924a77227237cb5b2bff955617bbda742dc7
Abe Storey <abester101@gmail.com>
2014-06-08 21:26:34 -0700

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imgapp::Application.config.secret_key_base = '24eef9edab53b87dae2f52b767c1156c44792e0349a3873827cb73e06472f16f53ca9930c029a45afea618b124c23c74a55dc605d7e307319cd331e2a2a73f0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imgapp::Application.config.session_store :cookie_store, key: '_imgapp_session'
